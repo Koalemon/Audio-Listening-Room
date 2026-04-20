ba7c75fb455e16a7f54f3cd96169172bce8b6e73
Koalemon <127819800+Koalemon@users.noreply.github.com>
2026-04-20 13:34:52 +0800

    Add files via upload

---
+++ pkg/wasm_security_bg.wasm.d.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_coreengine_free: (a: number, b: number) => void;
+export const coreengine_compute_bars: (a: number, b: number, c: number, d: number, e: number, f: number) => void;
+export const coreengine_fetch_and_process_playlist: (a: number) => number;
+export const coreengine_get_symmetric_spectrum: (a: number, b: number, c: number, d: number) => void;
+export const coreengine_get_visualizer_bars: (a: number, b: number, c: number, d: number) => void;
+export const coreengine_new: () => number;
+export const __wasm_bindgen_func_elem_234: (a: number, b: number, c: number, d: number) => void;
+export const __wasm_bindgen_func_elem_293: (a: number, b: number, c: number, d: number) => void;
+export const __wbindgen_export: (a: number, b: number) => number;
+export const __wbindgen_export2: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_export3: (a: number) => void;
+export const __wbindgen_export4: (a: number, b: number) => void;
+export const __wbindgen_add_to_stack_pointer: (a: number) => number;
+export const __wbindgen_export5: (a: number, b: number, c: number) => void;
